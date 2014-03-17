01d644f3e5f1df433b703996b8494ace27b30a9f
Bogdan <buhaibogdan@gmail.com>
2014-03-17 22:43:10 +0200

    Some controller refactoring.

--- src/OAuth/OAuthBundle/Controller/TokenController.php
+++ src/OAuth/OAuthBundle/Controller/TokenController.php
@@ -4,9 +4,11 @@ namespace OAuth\OAuthBundle\Controller;
 
 use OAuth\OAuthBundle\Exception\ClientNotFoundException;
 use OAuth\OAuthBundle\Exception\InvalidRefreshTokenException;
+use OAuth\OAuthBundle\Exception\UserNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
 
 
 class TokenController extends Controller
@@ -21,36 +23,19 @@ class TokenController extends Controller
             return new Response('POST method required.', Response::HTTP_METHOD_NOT_ALLOWED);
         }
 
-        // TODO: refactor all this !!
-
         $grantType = $request->request->get('grant_type');
 
         if ($grantType === static::GRANT_TYPE_PASSWORD) {
-            $username = $request->request->get('username');
-            $password = $request->request->get('password');
-            $clientId = $request->request->get('client_id');
-            $clientSecret = $request->request->get('client_secret');
-            if (empty($clientId) ||
-                empty($clientSecret) ||
-                empty($username) ||
-                empty($password)
-            ) {
-                return new Response('Fields missing from request. Check documentation.', Response::HTTP_BAD_REQUEST);
-            }
-            /** @var \Prodtrack\Bundle\Services\UserService $userService */
-            $userService = $this->get('prodtrack.user_service');
-            $user = $userService->getUserWithCredentials($username, $password);
-            if (is_null($user)) {
-                return new Response('Invalid username or password.', Response::HTTP_UNAUTHORIZED);
+            try {
+                $token = $this->getToken($request, $grantType);
+            } catch (UserNotFoundException $e) {
+                return new Response($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+            } catch (ClientNotFoundException $e) {
+                return new Response($e->getMessage(), Response::HTTP_UNAUTHORIZED);
+            } catch (MissingMandatoryParametersException $e) {
+                return new Response($e->getMessage(), Response::HTTP_BAD_REQUEST);
             }
 
-            try {
-                /** @var \OAuth\OAuthBundle\Services\ClientAuthenticator $auth */
-                $auth = $this->get('o_auth.authenticator');
-                $token = $auth->getTokenForClient($clientId, $clientSecret, $grantType);
-            } catch (ClientNotFoundException $e) {
-                return new Response('Invalid client credentials.', Response::HTTP_UNAUTHORIZED);
-            }
         } elseif ($grantType === static::GRANT_TYPE_REFRESH_TOKEN) {
             $refreshToken = $request->request->get('refresh_token');
             /** @var \OAuth\OAuthBundle\Services\ClientAuthenticator $auth */
@@ -67,5 +52,37 @@ class TokenController extends Controller
 
 
         return new Response(json_encode($token), 200, array('Content-type' => 'application/json'));
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param $grantType
+     * @throws \Symfony\Component\Routing\Exception\MissingMandatoryParametersException
+     * @throws \OAuth\OAuthBundle\Exception\UserNotFoundException
+     * @return array
+     */
+    protected function getToken(Request $request, $grantType)
+    {
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $clientId = $request->request->get('client_id');
+        $clientSecret = $request->request->get('client_secret');
+        if (empty($clientId) ||
+            empty($clientSecret) ||
+            empty($username) ||
+            empty($password)
+        ) {
+            throw new MissingMandatoryParametersException('Fields missing from request. Check documentation.');
+        }
+        /** @var \Prodtrack\Bundle\Services\UserService $userService */
+        $userService = $this->get('prodtrack.user_service');
+        $user = $userService->getUserWithCredentials($username, $password);
+        if (is_null($user)) {
+            throw new UserNotFoundException('Invalid username or password.');
+        }
+
+        /** @var \OAuth\OAuthBundle\Services\ClientAuthenticator $auth */
+        $auth = $this->get('o_auth.authenticator');
+        return $auth->getTokenForClient($clientId, $clientSecret, $grantType);
     }
 }
