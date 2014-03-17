ea42b60ed98cee1e92cda73ca61b65fa780e4d7f
Bogdan <buhaibogdan@gmail.com>
2014-03-17 23:22:29 +0200

    Class for creating response errors.

---
+++ src/OAuth/OAuthBundle/Services/ErrorResponse.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace OAuth\OAuthBundle\Services;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ErrorResponse
+{
+    protected $invalidTokenMsg =
+        'Bearer "prodTrack", error="invalid_token", error_description="Your token has expired."';
+
+    protected $invalidRequestMsg =
+        'Bearer "prodTrack", error="invalid_request", error_description="Some params might be missing. Check the docs"';
+
+    protected $invalidClientMsg =
+        'Bearer "prodTrack", error="invalid_client", error_description="Client authentication failed."';
+
+    protected $unsupportedGrantType =
+        'Bearer "prodTrack", error="unsupported_grant_type"';
+
+    /**
+     * @return Response
+     */
+    public function getExpiredTokenResponse()
+    {
+        return new Response(
+            '',
+            Response::HTTP_UNAUTHORIZED,
+            array('WWW-Authenticate' => $this->invalidTokenMsg)
+        );
+    }
+
+    public function getInvalidRequestResponse()
+    {
+        return new Response(
+            '',
+            Response::HTTP_BAD_REQUEST,
+            array('WWW-Authenticate' => $this->invalidRequestMsg)
+        );
+    }
+
+    public function getInvalidClientResponse()
+    {
+        return new Response(
+            '',
+            Response::HTTP_UNAUTHORIZED,
+            array('WWW-Authenticate' => $this->invalidClientMsg)
+        );
+    }
+
+    public function getUnsupportedGrantTypeRequest()
+    {
+        return new Response(
+            '',
+            Response::HTTP_BAD_REQUEST,
+            array('WWW-Authenticate' => $this->unsupportedGrantType)
+        );
+    }
+
+}
---
+++ src/OAuth/OAuthBundle/Tests/Services/ErrorResponseTest.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace OAuth\OAuthBundle\Tests\Services;
+
+
+use OAuth\OAuthBundle\Services\ErrorResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class ErrorResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetExpiredTokenResponse()
+    {
+        $errorMsg = 'Bearer "prodTrack", error="invalid_token", error_description="Your token has expired."';
+
+        $err = new ErrorResponse();
+        $invalidToken = $err->getExpiredTokenResponse();
+        $status = $invalidToken->getStatusCode();
+        $header = $invalidToken->headers->get('WWW-Authenticate');
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $invalidToken);
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $status);
+        $this->assertEmpty($invalidToken->getContent());
+        $this->assertEquals($header, $errorMsg);
+    }
+
+
+    public function testGetInvalidRequest()
+    {
+        $errorMsg = 'Bearer "prodTrack", error="invalid_request", ' .
+            'error_description="Some params might be missing. Check the docs"';
+
+        $err = new ErrorResponse();
+        $invalidToken = $err->getInvalidRequestResponse();
+        $status = $invalidToken->getStatusCode();
+        $header = $invalidToken->headers->get('WWW-Authenticate');
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $invalidToken);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $status);
+        $this->assertEmpty($invalidToken->getContent());
+        $this->assertEquals($header, $errorMsg);
+    }
+
+    public function testGetInvalidClientRequest()
+    {
+        $errorMsg =
+            'Bearer "prodTrack", error="invalid_client", error_description="Client authentication failed."';
+
+        $err = new ErrorResponse();
+        $invalidToken = $err->getInvalidClientResponse();
+        $status = $invalidToken->getStatusCode();
+        $header = $invalidToken->headers->get('WWW-Authenticate');
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $invalidToken);
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $status);
+        $this->assertEmpty($invalidToken->getContent());
+        $this->assertEquals($header, $errorMsg);
+    }
+
+    public function testGetUnsupportedGrantTypeRequest()
+    {
+        $errorMsg =
+            'Bearer "prodTrack", error="unsupported_grant_type"';
+
+        $err = new ErrorResponse();
+        $invalidToken = $err->getUnsupportedGrantTypeRequest();
+        $status = $invalidToken->getStatusCode();
+        $header = $invalidToken->headers->get('WWW-Authenticate');
+
+        $this->assertInstanceOf('\Symfony\Component\HttpFoundation\Response', $invalidToken);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $status);
+        $this->assertEmpty($invalidToken->getContent());
+        $this->assertEquals($header, $errorMsg);
+    }
+
+}
